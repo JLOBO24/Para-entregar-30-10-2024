ed75bde32ebf39e9087ab2b6b31bc95d7af253fc
PROGRAMAÇÃO EM PYTHON 2024.4 <51129532024.4@senacrjedu.local>
2024-10-30 20:06:37 -0300

    Exercícios 30-10-2024

---
+++ estruturadecisao.js
@@ -0,0 +1,76 @@
+let valor = 10
+
+if ( valor >= 10) { 
+    console.log("Você possui madeiras suficiente! ")
+
+} else { 
+    console.log("Você possui madeiras suficiente! ")
+
+
+}
+
+
+//.............................................//..........................
+
+
+let numero = 10
+
+
+if ( numero < 0 ) {
+     console.log("valor negativo")
+}else if ( numero == 0 ) {
+    console.log("O valor é igual a zero")
+} else if (numero >= 0 ) {
+    console.log("Valor positivo")
+    
+}else {
+    console.log("Valor desconhecido!")
+}
+//..........................................//................................
+
+//Exercício 1 - 30-10-2024
+
+let numero_um = 57
+
+if ( numero_um % 2 == 0 ) {
+    console.log("mumero par")
+
+}else {
+    console.log("numero ímpar")
+}
+
+//........................................//.....................................
+
+//Exercício 2 - 30-10-2024
+
+let nota1 = 5
+let nota2 = 5
+
+if (((nota1 + nota2)/2) >= 6 ) {
+    console.log("aluno aprovado")
+}else if (((nota1 + nota2) / 2) > 4 && ((nota1 + nota2) / 2) < 5.9  ) {
+    console.log("aluno em recuperação")
+}else {
+    console.log("aluno reprovado")
+}
+
+//.........................................//......................................
+
+//Exercício 3 - 30-10-2024
+
+let lado1 = 7
+let lado2 = 7
+let lado3 = 7
+
+if (lado1 = lado2 = lado3) {
+    console.log("triângulo equilátero")
+
+
+}else if ((lado1 = lado2) || (lado1 = lado3) || (lado2 = lado3)) {
+    console.log("triangulo isósceles")
+
+}else {
+    console.log("triângulo escaleno")
+}
+
+//.........................................//......................................
